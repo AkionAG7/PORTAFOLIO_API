aa4ba82b95125c2929d47fc68b7dadecf81601cf
AkionAG7 <akion.cheng.jimenez@est.una.ac.cr>
2026-03-02 21:37:04 -0600

    FEAT: Implementacion de buckets de supabase para imagenes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from app.routers import auth,user
+from app.routers import auth,user, contact
 
 app = FastAPI(title="Portafolio API", version="1.0.0")
 
 app.include_router(auth.router)
 app.include_router(user.router)
+app.include_router(contact.router)
 
 
 @app.get("/", tags=["Health"]) 
