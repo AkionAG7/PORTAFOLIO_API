6c2bb9669afbd34a01a424ddb79d6e1b37869d16
AkionAG7 <akion.cheng.jimenez@est.una.ac.cr>
2026-04-06 21:49:07 -0600

    Fix: Add cors allows for any user

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, user, contact, language
 from app.routers import stack, project
 
 app = FastAPI(title="Portafolio API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(user.router)
